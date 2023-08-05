301b99d17376f20aef677bfdd3568bfbee4c3365
confidence8492 <115582886+confidence8492@users.noreply.github.com>
2023-08-05 22:16:08 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
-function test(){
-  var script=document.createElement("script");
-  script.src="https://code.jquery.com/jquery-3.7.0.min.js"
-  document.head.appendChild(script)
+var script=document.createElement("script");
+script.src="https://code.jquery.com/jquery-3.7.0.min.js"
+document.head.appendChild(script)
+$(".div").on("click",function(){
   $(".div").hide();
-}
+});
