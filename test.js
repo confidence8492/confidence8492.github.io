3dc4cb888ef6b1f3767ec32de4a57e1d5415ba49
confidence8492 <115582886+confidence8492@users.noreply.github.com>
2023-08-05 21:26:29 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,6 @@
+var script=document.createElement("script");
+script.src="https://code.jquery.com/jquery-3.7.0.min.js"
+document.head.appendChild(script)
 function test(){
-  prompt(1);
+  $(".div").hide();
 }
